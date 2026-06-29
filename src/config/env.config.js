02e121a20fe6ecb71d11f2f24fd69c6b1547aeb0
rakibulrahmandev <rakibulrahman.webdev@gmail.com>
2026-06-29 18:36:36 +0600

    feat: implement env configuration

--- src/config/env.config.js
+++ src/config/env.config.js
@@ -1,1 +1,13 @@
-// TODO: ENV CONFIGURATION
+import 'dotenv/config';
+
+const configure = {
+    port: process.env.PORT,
+    node_env: process.env.NODE_ENV,
+    database_uri: process.env.DATABASE_URI,
+    cors_origin: process.env.CORS_ORIGIN,
+    jwt_secret: process.env.JWT_SECRET,
+};
+
+const config = Object.freeze(configure);
+
+export default config;
