c453065c75f8fab68154a6c030b0b34162142bb2
rakibulrahmandev <rakibulrahman.webdev@gmail.com>
2026-06-29 18:49:05 +0600

    feat: implement database configuration

--- src/config/db.config.js
+++ src/config/db.config.js
@@ -1,1 +1,23 @@
-// TODO: DATABASE CONFIGURATION
+import mongoose from 'mongoose';
+import config from './env.config.js';
+
+const dbConfig = async () => {
+    try {
+        const uri = config.database_uri;
+
+        await mongoose.connection.on('connected', () => {
+            console.log('mongoose connection successfully');
+        });
+
+        await mongoose.connection.on('error', (err) => {
+            console.log('mongoose connection failure', err.message);
+        });
+
+        await mongoose.connect(uri);
+    } catch (error) {
+        console.log('mongoose connection error', error.message);
+        process.exit(1);
+    }
+};
+
+export default dbConfig;
