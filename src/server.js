3c908a5d40e7f8c1ee357869abaceaf1a94dfefe
rakibulrahmandev <rakibulrahman.webdev@gmail.com>
2026-06-29 19:24:24 +0600

    feat: initialize server with database connection

--- src/server.js
+++ src/server.js
@@ -1,1 +1,19 @@
-// TODO: SERVER START HERE
+import app from './app.js';
+import dbConfig from './config/db.config.js';
+import config from './config/env.config.js';
+
+const server = async () => {
+    try {
+        const port = config.port || 5000;
+
+        await dbConfig();
+
+        app.listen(port, () => {
+            console.log(`server running on http://localhost:${port}`);
+        });
+    } catch (error) {
+        console.log(error.message);
+    }
+};
+
+server();
