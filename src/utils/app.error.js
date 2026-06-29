66ecd3386d35c05fdbb3ba17f11f09e9502e0084
rakibulrahmandev <rakibulrahman.webdev@gmail.com>
2026-06-29 22:49:08 +0600

    feat: add AppError custom error class

--- src/utils/app.error.js
+++ src/utils/app.error.js
@@ -1,1 +1,11 @@
-// TODO: APP ERROR
+class AppError extends Error {
+    constructor(statusCode, message) {
+        super(message);
+
+        this.statusCode = statusCode;
+
+        Error.captureStackTrace(this, this.constructor);
+    }
+}
+
+export default AppError;
