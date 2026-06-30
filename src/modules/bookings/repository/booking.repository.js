28505598bf8e912db5e243a8e1b663df53832dde
rakibulrahmandev <rakibulrahman.webdev@gmail.com>
2026-07-01 01:36:54 +0600

    feat: add facility and booking repositories

--- src/modules/bookings/repository/booking.repository.js
+++ src/modules/bookings/repository/booking.repository.js
@@ -1,1 +1,25 @@
-// TODO: BOOKING REPOSITORY
+import Booking from '../model/booking.model.js';
+
+export const createBooking = async (payload) => {
+    return await Booking.create(payload);
+};
+
+export const readBookings = async () => {
+    return await Booking.find().lean();
+};
+
+export const readBookingById = async (id) => {
+    return await Booking.findById(id).lean();
+};
+
+export const updateBookingById = async (id, payload) => {
+    return await Booking.findByIdAndUpdate(
+        id,
+        {
+            $set: payload,
+        },
+        {
+            returnDocument: 'after',
+        }
+    );
+};
--- src/modules/facilities/repository/facilities.repository.js
+++ src/modules/facilities/repository/facilities.repository.js
@@ -1,1 +1,32 @@
-// TODO: FACILITIES REPOSITORY
+import Facility from '../model/facilities.model.js';
+
+export const createFacility = async (payload) => {
+    return await Facility.create(payload);
+};
+
+export const readFacilities = async () => {
+    return await Facility.find().lean();
+};
+
+export const readFacilityById = async (id) => {
+    return await Facility.findById(id).lean();
+};
+
+export const updateFacilityById = async (id, payload) => {
+    return await Facility.findByIdAndUpdate(
+        id,
+        {
+            $set: payload,
+        },
+        {
+            returnDocument: 'after',
+            runValidators: true,
+        }
+    );
+};
+
+export const deleteFacilityById = async (id) => {
+    return await Facility.findByIdAndDelete(id, {
+        returnDocument: 'after',
+    });
+};
