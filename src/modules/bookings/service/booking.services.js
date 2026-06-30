becf5fd87050d6f47c90d1a9bd546ade57477ad7
rakibulrahmandev <rakibulrahman.webdev@gmail.com>
2026-06-30 12:56:01 +0600

    feat: add facility and booking services

--- src/modules/bookings/service/booking.services.js
+++ src/modules/bookings/service/booking.services.js
@@ -1,1 +1,20 @@
-// TODO: BOOKING SERVICE
+/* eslint-disable no-unused-vars */
+
+import Booking from '../model/booking.model.js';
+import AppError from '../../../utils/app.error.js';
+
+export const createBookingService = async (payload) => {
+    console.log('create booking service call');
+};
+
+export const readsBookingService = async () => {
+    console.log('reads booking service call');
+};
+
+export const readBookingService = async () => {
+    console.log('read booking service');
+};
+
+export const updateBookingService = async () => {
+    console.log('update booking service');
+};
--- src/modules/facilities/service/facilities.services.js
+++ src/modules/facilities/service/facilities.services.js
@@ -1,1 +1,24 @@
-// TODO: FACILITIES SERVICES
+/* eslint-disable no-unused-vars */
+
+import Facility from '../model/facilities.model.js';
+import AppError from '../../../utils/app.error.js';
+
+export const createFacilityService = async (payload) => {
+    console.log('create facility service call');
+};
+
+export const readsFacilityService = async () => {
+    console.log('reads facilities service call');
+};
+
+export const readFacilityService = async (id) => {
+    console.log('read facility service call');
+};
+
+export const updateFacilityService = async (id, payload) => {
+    console.log('update facility service call');
+};
+
+export const deleteFacilityService = async (id) => {
+    console.log('delete facility service call');
+};
