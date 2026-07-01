4698dcc0fecc37e26b51d2a7bc3a05f2eb24a2c8
rakibulrahmandev <rakibulrahman.webdev@gmail.com>
2026-07-01 10:30:24 +0600

    feat: add error middleware and implement as a global error handler

--- src/app.js
+++ src/app.js
@@ -3,6 +3,8 @@ import express from 'express';
 import facilityRouter from './routes/facility.route.js';
 import bookingRouter from './routes/booking.route.js';
 
+import errorMiddleware from './middlewares/error.middleware.js';
+
 const app = express();
 
 app.use(express.json());
@@ -10,5 +12,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use('/api/v1', facilityRouter);
 app.use('/api/v1', bookingRouter);
+
+app.use(errorMiddleware);
 
 export default app;
--- src/middlewares/error.middleware.js
+++ src/middlewares/error.middleware.js
@@ -1,1 +1,17 @@
-// TODO: ERROR MIDDLEWARE
+/* eslint-disable no-unused-vars */
+
+import config from '../config/env.config.js';
+
+const errorMiddleware = (err, req, res, next) => {
+    let statusCode = err.statusCode || 500;
+    let message = err.message || 'Internal server error';
+
+    res.status(statusCode).res.json({
+        success: false,
+        status: statusCode,
+        message: message,
+        stack: config.node_env === 'development' ? err.stack : null,
+    });
+};
+
+export default errorMiddleware;
